de4ba10fcae5587ea54f336080953c5d436cc830
zxcpisya <mamin.papa0@gmail.com>
2024-12-24 23:37:14 +1000

    add sockets to send invitation to game

--- backend/gameapp/routing.py
+++ backend/gameapp/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/game/(?P<game_id>\d+)/$', consumers.GameConsumer.as_asgi()),
     re_path(r'ws/home/', consumers.ShowActiveGamesConsumer.as_asgi()),
+    re_path(r'ws/waiting_request/(?P<username>\w+)/$', consumers.SendGameRequestConsumer.as_asgi()),
 ]
