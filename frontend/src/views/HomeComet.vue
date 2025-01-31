b5462cde20b01a8640819c0d9f527ddfd0458980
zxcpisya <mamin.papa0@gmail.com>
2025-02-01 00:18:46 +1000

    clear console logs

--- frontend/src/views/HomeComet.vue
+++ frontend/src/views/HomeComet.vue
@@ -60,11 +60,13 @@ export default {
       listnerID,
       new CometChat.CallListener({
         onIncomingCallReceived(call) {
+          console.log("Incoming call:", call);
           globalContext.incomingCall = true;
           globalContext.session_id = call.sessionId;
         },
 
         onOutgoingCallAccepted(call) {
+          console.log("Outgoing call accepted:", call);
           globalContext.ongoingCall = true;
           CometChat.startCall(
             call.sessionId,
@@ -72,16 +74,19 @@ export default {
             new CometChat.OngoingCallListener({
               onUserJoined: user => {
                 /* Notification received here if another user joins the call. */
+                console.log("User joined call:", user);
                 /* this method can be use to display message or perform any actions if someone joining the call */
               },
               onUserLeft: user => {
                 /* Notification received here if another user left the call. */
+                console.log("User left call:", user);
                 /* this method can be use to display message or perform any actions if someone leaving the call */
               },
               onCallEnded: call => {
                 globalContext.ongoingCall = false;
                 globalContext.incomingCall = false;
                 /* Notification received here if current ongoing call is ended. */
+                console.log("Call ended:", call);
                 /* hiding/closing the call screen can be done here. */
               }
             })
@@ -89,12 +94,14 @@ export default {
           // Outgoing Call Accepted
         },
         onOutgoingCallRejected(call) {
+          console.log("Outgoing call rejected:", call);
           this.incomingCall = false;
           this.ongoingCall = false;
           this.receiver_id = "";
           // Outgoing Call Rejected
         },
         onIncomingCallCancelled(call) {
+          console.log("Incoming call calcelled:", call);
         }
       })
     );
